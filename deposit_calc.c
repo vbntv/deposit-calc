1a8f252f0e215b949e00f04b4bbf413edfb262eb
vbntv <vbuntovv@mail.ru>
2017-03-06 05:21:33 +0700

    Реализация ввода данных пользователем с клавиатуры

--- deposit_calc.c
+++ deposit_calc.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() 
+{
+    int srok;
+    float vklad;
+
+    printf("Срок: ");        
+    scanf("%d", &srok);
+    printf("Сумма вклада: ");
+    scanf("%f", &vklad);
+}
